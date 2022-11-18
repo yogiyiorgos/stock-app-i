b95b23fdab78dc133f60154ef914029e9a4ca7a9
yogiyiorgos <georgefil@outlook.com>
2022-11-18 16:25:13 +0100

    Build

--- src/apis/finnHub.js
+++ src/apis/finnHub.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
   baseURL: 'https://finnhub.io/api/v1',
   params: {
-    token: 'cccdljiad3i4bak96clg',
+    token: 'cdrq1j2ad3icpsta0aogcdrq1j2ad3icpsta0ap0',
   },
 })
